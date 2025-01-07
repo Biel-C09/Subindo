345408abe415abeac8cc2d54d83f239fc0540237
Biel-C09 <103579126+Biel-C09@users.noreply.github.com>
2025-01-06 23:38:15 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,11 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-
 var boneco;
 
-function preload(){
-
-}
-
 function setup() {
-    canvas = createCanvas(displayWidth, displayHeight);
-    engine = Engine.create();
-    world = engine.world;
+    createCanvas(600,400);
 
- boneco = createSprite(width+10,height+10); 
- boneco.scale = 0.50;
- boneco.setCollider("circle", 0,0,227);
- boneco.visible = true;
+ boneco = createSprites(100,100,100,100); 
 }   
 
 function draw() {
-    background(bg_Img,0,0,width,height);
-    Engine.update(engine);
-  drawSprites()
-}
+  drawSprites();
+} 
